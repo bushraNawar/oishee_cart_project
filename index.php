8707423995f9ba3421357dc2161bcf8f6c909ff6
Bushra <bushranawar17@gmail.com>
2021-06-13 03:11:33 +0600

    added home page slider to home page

--- index.php
+++ index.php
@@ -1,6 +1,55 @@
     <?php include('header.php'); ?>
 
-   
+   <div class="container">
+ 
+  <div id="myCarousel" class="carousel slide" data-ride="carousel">
+    <!-- Indicators -->
+    <ol class="carousel-indicators">
+      <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+      <li data-target="#myCarousel" data-slide-to="1"></li>
+      <li data-target="#myCarousel" data-slide-to="2"></li>
+    </ol>
+
+    <!-- Wrapper for slides -->
+    <div class="carousel-inner">
+
+      <div class="item active">
+       <a href="details.php?productId=3"> <img src="assets/images/demo.jpg" alt="" style="width:100%;"></a>
+        <div class="carousel-caption">
+          <h3>Product</h3>
+        <a href="details.php?productId=3" class="btn btn-danger">BUY NOW</a>
+        </div>
+      </div>
+
+      <div class="item">
+        <a href="details.php?productId=3"> <img src="assets/images/demo.jpg" alt="" style="width:100%;"></a>
+        <div class="carousel-caption">
+          <h3>Product</h3>
+        <a href="details.php?productId=3" class="btn btn-danger">BUY NOW</a>
+        </div>
+      </div>
+    
+      <div class="item">
+       <a href="details.php?productId=3"> <img src="assets/images/demo.jpg" alt="" style="width:100%;"></a>
+        <div class="carousel-caption">
+          <h3>Product</h3>
+           <a href="details.php?productId=3" class="btn btn-danger">BUY NOW</a>
+        </div>
+      </div>
+  
+    </div>
+
+    <!-- Left and right controls -->
+    <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+      <span class="glyphicon glyphicon-chevron-left"></span>
+      <span class="sr-only">Previous</span>
+    </a>
+    <a class="right carousel-control" href="#myCarousel" data-slide="next">
+      <span class="glyphicon glyphicon-chevron-right"></span>
+      <span class="sr-only">Next</span>
+    </a>
+  </div>
+</div>
 
       <!--Slider        Ends  -->
     <!--Featured product -->
